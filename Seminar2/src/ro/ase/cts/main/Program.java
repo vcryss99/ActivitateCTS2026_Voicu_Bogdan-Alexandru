c0ca3b89eb710a031d1aef0848932ec3c189c408
vcryss99 <voicubogdan2018@yahoo.com>
2026-03-09 20:38:00 +0200

    Seminar2

---
+++ Seminar2/src/ro/ase/cts/main/Program.java
@@ -0,0 +1,26 @@
+package ro.ase.cts.main;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readere.AngajatReader;
+import ro.ase.cts.clase.readere.AplicantReader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+		try {
+			AplicantReader angajatReader = new AngajatReader();
+			listaAngajati = angajatReader.readAplicant("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
